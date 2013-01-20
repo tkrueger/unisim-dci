407f3ca0bdcbb4b8a7f226bf2a8d0f5397e441b8
Thorsten Krüger <thorstenkg@googlemail.com>
2013-01-20 14:15:37 +0100

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnisimDci::Application.config.secret_token = '62ff371c529e89989b853128d617995b0d91ba3860a269237c7a4f117b9622e1c12b349decebcdbf27484e3c93afd22a1cf3f7f1934be7f56d6b6737dbce9110'
